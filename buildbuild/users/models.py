4a5793ff09484048201b47bc53c7e9fd059e8165
dobestan <dobestan@gmail.com>
2014-07-28 21:46:31 +0900

    basic user model : with email and password

--- buildbuild/users/models.py
+++ buildbuild/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(
+                verbose_name = "Email Address",
+                max_length = 50,
+                unique = True
+            )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
--- buildbuild/users/tests.py
+++ buildbuild/users/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from users.models import User
+from django.db import IntegrityError
 
-# Create your tests here.
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.user = User()
+        pass
+
+    # Email
+    def test_user_should_have_email_field(self):
+        try:
+            self.user.email
+        except AttributeError:
+            self.fail("User should have email field")
+
+    def test_user_should_have_unique_email(self):
+        unique_email = "username@organization.org"
+        self.user.email = unique_email
+        self.user.save()
+
+        user_with_duplicate_email = User(email = self.user.email)
+        self.assertRaises(IntegrityError, user_with_duplicate_email.save)
+
+    # Password
+    def test_user_should_have_password_field(self):
+        try:
+            self.user.password
+        except AttributeError:
+            self.fail("User should have password field")

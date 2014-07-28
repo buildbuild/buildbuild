dbd7e85b31f8307caf2ad9dd874d74e6f3009d3a
dobestan <dobestan@gmail.com>
2014-07-28 23:21:44 +0900

    update user model : password length should be at least 6

--- buildbuild/users/models.py
+++ buildbuild/users/models.py
@@ -3,17 +3,24 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
 from django.core.validators import validate_email
 
+from django.core.exceptions import ValidationError
+
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password):
         if not email:
             raise ValueError("User must have an email address")
 
         validate_email(email)
+        self.validate_password(password)
 
         user = self.model(email = self.normalize_email(email))
         user.set_password(password)
         user.save(using = self._db)
         return user
+
+    def validate_password(self, password):
+        if len(password) < 6:
+            raise ValidationError("user password length should be at least 6")
 
 class User(AbstractBaseUser):
     email = models.CharField(
--- buildbuild/users/tests.py
+++ buildbuild/users/tests.py
@@ -7,8 +7,8 @@ from django.core.exceptions import ValidationError
 class UserModelTest(TestCase):
     def setUp(self):
         self.user = User()
-        self.unique_email = "username@organization.org"
-        self.password = "foobar"
+        self.valid_email = "username@organization.org"
+        self.valid_password = "foobar"
         pass
 
     # Email
@@ -20,19 +20,23 @@ class UserModelTest(TestCase):
 
     def test_user_email_should_not_be_blank(self):
         self.user.email = ""
+        self.user.password = self.valid_password
         self.assertRaises(ValueError, self.user.save())
 
     def test_user_should_have_unique_email(self):
-        self.user.email = self.unique_email
+        self.user.email = self.valid_email
+        self.user.password = self.valid_password
         self.user.save()
 
-        user_with_duplicate_email = User(email = self.user.email)
+        user_with_duplicate_email = User(email = self.user.email, password = self.valid_password)
 
         self.assertRaises(IntegrityError, user_with_duplicate_email.save)
 
     def test_user_with_invalid_email_should_not_be_valid(self):
-        self.assertRaises(ValidationError, User.users.create_user, email = "INVALID_EMAIL")
-
+        self.assertRaises(ValidationError, User.users.create_user,
+                email = "INVALID_EMAIL",
+                password = self.valid_password
+                )
 
     # Password
     def test_user_should_have_password_field(self):
@@ -41,22 +45,21 @@ class UserModelTest(TestCase):
         except AttributeError:
             self.fail("User should have password field")
 
-    # UserManager
-    def test_user_only_with_email_should_be_created_via_user_manager(self):
-        try:
-            user = User.users.create_user(email = self.unique_email)
-        except:
-            self.fail("User should be created via UserManager")
+    def test_user_with_short_password_should_not_be_valid(self):
+        self.assertRaises(ValidationError, User.users.create_user,
+                email = self.valid_email,
+                password = "foo" )
 
+    # UserManager
     def test_user_should_be_created_via_user_manager(self):
         try:
             user = User.users.create_user(
-                    email = self.unique_email,
-                    password = self.password
+                    email = self.valid_email,
+                    password = self.valid_password,
                     )
         except:
             self.fail("User should be created via UserManager")
 
     def test_user_password_should_be_encrypted(self):
-        user = User.users.create_user( email = self.unique_email, password = self.password )
-        self.assertNotEqual(self.password, user.password)
+        user = User.users.create_user( email = self.valid_email, password = self.valid_password )
+        self.assertNotEqual(self.valid_password, user.password)

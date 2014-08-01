ebdb8413bc29ab835659377110dca015bee07040
riskkim <wnsgh611@gmail.com>
2014-08-02 01:00:16 +0900

    add password field into REQUIRED_FIELDS

--- buildbuild/users/models.py
+++ buildbuild/users/models.py
@@ -55,7 +55,7 @@ class User(AbstractBaseUser):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['password']
 
     def deactivate(self):
         self.is_active = False

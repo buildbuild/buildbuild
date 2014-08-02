7fb43f175855afc64cfc1fdd3019e63c10a161c3
riskkim <wnsgh611@gmail.com>
2014-08-02 21:43:54 +0900

    complete # 2 test code for delete_user

--- buildbuild/users/tests/test_user_manager.py
+++ buildbuild/users/tests/test_user_manager.py
@@ -7,7 +7,7 @@ class TestUserManager(TestCase):
         self.user = User()
         self.valid_email = "test@example.com"
         self.valid_password = "test_password"
-        self.invalid_email = "nouser@example.com"
+        self.not_created_user_email = "nouser@example.com"
 
     def test_user_should_be_created_via_user_manager(self):
         try:
@@ -19,27 +19,30 @@ class TestUserManager(TestCase):
             self.fail("User should be created via UserManager")
 
     def test_get_user_should_return_user_via_user_manager(self):
-        _user = User()
-        _user.email = self.valid_email
-        _user.password = self.valid_password
-        _user.save()
+        _user = User.objects.create_user(
+                    email = self.valid_email,
+                    password = self.valid_password,
+                    )
         try:
             user = User.objects.get_user(
-                    email = self.valid_email
+                    email = _user.email
                     )
         except:
             self.fail("UserManager should return user via get_user")
 
     def test_get_user_with_invalid_email_should_be_fail(self):
         self.assertRaises(ObjectDoesNotExist, User.objects.get_user,
-                          email=self.invalid_email
+                          email=self.not_created_user_email
                         )
 
     def test_delete_user_must_disable_is_active_via_user_manager(self):
-        _user = User()
-        _user.email = self.valid_email
-        _user.password = self.valid_password
-        _user.save()
-        User.objects.delete_user(self.valid_email)
-        self.assertEqual(False, self.user.is_active,
+        user = User.objects.create_user(
+                    email = self.valid_email,
+                    password = self.valid_password,
+                    )
+        User.objects.delete_user(user.email)
+        user = User.objects.get_user(
+                    email = self.valid_email
+                )
+        self.assertEqual(False, user.is_active,
                          "is_active should be False")

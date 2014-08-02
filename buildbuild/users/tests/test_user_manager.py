a1eea04e55f586f7af58b987814bd82a754fd996
riskkim <wnsgh611@gmail.com>
2014-08-02 21:04:54 +0900

    add test code for Retrieve user(get_user)

--- buildbuild/users/tests/test_user_manager.py
+++ buildbuild/users/tests/test_user_manager.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
 from users.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 class TestUserManager(TestCase):
     def setUp(self):
         self.user = User()
         self.valid_email = "test@example.com"
         self.valid_password = "test_password"
+        self.invalid_email = "nouser@example.com"
 
     def test_user_should_be_created_via_user_manager(self):
         try:
@@ -15,3 +17,20 @@ class TestUserManager(TestCase):
                     )
         except:
             self.fail("User should be created via UserManager")
+
+    def test_get_user_should_return_user_via_user_manager(self):
+        _user = User()
+        _user.email = self.valid_email
+        _user.password = self.valid_password
+        _user.save()
+        try:
+            user = User.objects.get_user(
+                    email = self.valid_email
+                    )
+        except:
+            self.fail("UserManager should return user via get_user")
+
+    def test_get_user_with_invalid_email_should_be_fail(self):
+        self.assertRaises(ObjectDoesNotExist, User.objects.get_user,
+                          email=self.invalid_email
+                        )

a50195060cd51f7d4ef70138d07206dd72e86d6d
dobestan <dobestan@gmail.com>
2014-08-07 00:06:17 +0900

    add non-app-specific templates

--- buildbuild/buildbuild/settings.py
+++ buildbuild/buildbuild/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -23,6 +22,10 @@ SECRET_KEY = 'z84x8kvgq3oxtz%p@_m_td!d_@ebx-*b&4n$ad-*he@nhusni)'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates').replace('\\','/'),
+)
 
 ALLOWED_HOSTS = []
 

f68d256d83decf50dc1f0a84617d2ab770132b5b
riskkim <wnsgh611@gmail.com>
2014-08-07 17:58:20 +0900

    add urls for specific user detail info listing

--- buildbuild/users/urls.py
+++ buildbuild/users/urls.py
@@ -3,7 +3,8 @@ from users import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
-    url(r'/list/$', views.ListAllView.as_view(), name='listAll'),
+    url(r'^list/$', views.ListAllView.as_view(), name='listAll'),
+    url(r'^list/(?P<pk>\d+)/$', views.ListOneView.as_view(), name='listOne'),
     #url(r'^users/', include('users.urls')),
 )
 

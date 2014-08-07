0f497841df049c31d0643cf38c7c27e3ceb5da5d
riskkim <wnsgh611@gmail.com>
2014-08-07 13:46:34 +0900

    add views.py for class based view (List View)

--- buildbuild/users/views.py
+++ buildbuild/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.generic.list import ListView
+from users.models import User, UserManager
 # Create your views here.
+
+def home(request):
+    return render(request, 'users/home.html')
+
+class ListAllView(ListView):
+
+    template_name = 'users/listAll.html'
+    context_object_name = 'userList'
+
+    def get_queryset(self):
+        """Return the all signed users."""
+        return User.objects.order_by('id')

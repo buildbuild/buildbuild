781d383a6439676c75aa1d58e9da56c3a54a49f8
riskkim <wnsgh611@gmail.com>
2014-08-07 17:59:09 +0900

    add views.py for list specific user information

--- buildbuild/users/views.py
+++ buildbuild/users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic import DetailView
 from django.views.generic.edit import FormView
 from django.contrib.auth import login
 from django.contrib.auth import authenticate
@@ -10,16 +11,23 @@ from users.forms import LoginForm
 from users.models import User
 
 def home(request):
-    return render(request, 'users/home.html')
+    if request.method == 'GET':
+        return render(request, 'users/home.html')
+    elif request.method == 'POST':
+        return redirect("/users/list/" + request.POST['id'])
 
 class ListAllView(ListView):
-
     template_name = 'users/listAll.html'
     context_object_name = 'userList'
 
     def get_queryset(self):
         """Return the all signed users."""
         return User.objects.order_by('id')
+
+class ListOneView(DetailView):
+    template_name = 'users/listOne.html'
+    model = User
+    context_object_name = 'user'
 
 class Login(FormView):
     template_name = "users/login.html"

2a63020d8dc2ad217e017906d1056e10845558bc
wikibootup <wikibootup@gmail.com>
2014-08-07 15:34:25 +0900

    team init2

--- buildbuild/teams/models.py
+++ buildbuild/teams/models.py
@@ -1,69 +1,37 @@
 from django.db import models
 from users.models import User
+from django.core.exceptions import ValidationError
+
 # Create your models here.
 
 class TeamManager(models.Manager):
-	def create_team(self):
-		pass
-#	
+	def create_team(self, **kwargs):
+		team = self.model()
+	
+		if "name" in kwargs:
+			team.name = kwargs["name"]
+
+		team.save(using = self._db)
+		return team
 
 class Team(models.Model):
-	Team = TeamManager()
-	teamName = models.Charfield(max_length = 50)
-
-	def check_team_name_(self, teamName):
-		return teamName
+	"""
+	team model functions
+	- team_name
+	- team_position
+	- team_permission
+	- team_sign_up_form
+	- team_email
+	- team_contacet_number
+	- team_website_url
 	
+	"""
+	
+	objects = TeamManager()
+	name = models.CharField(max_length = 30, default = "team name is required")
+#	team_email = models.EmailField(
+#			verbose_name = "team Email Address",
+#			max_length = 50,
+#			unique = True,
+#			)
 		
-
-
-#from django.db import models
-#from django.contrib.auth.models import AbstractBaseUser
-#from django.contrib.auth.models import BaseUserManager
-#from django.core.validators import validate_email
-#
-#from django.core.exceptions import ValidationError
-#
-#class UserManager(BaseUserManager):
-#    def create_user(self, email, password, **kwargs):
-#        if not email:
-#            raise ValueError("User must have an email address")
-#
-#
-#        validate_email(email)
-#        self.validate_password(password)
-#
-#        user = self.model(email = self.normalize_email(email))
-#        user.set_password(password)
-#
-#        if "name" in kwargs:
-#            user.name = kwargs["name"]
-#
-#        if "is_admin" in kwargs and kwargs["is_admin"]:
-#            user.is_admin = True
-#
-#        if "is_org_admin" in kwargs and kwargs["is_org_admin"]:
-#            user.is_org_admin = True
-#
-#        user.save(using = self._db)
-#        return user
-#
-#    def validate_password(self, password):
-#        if len(password) < 6:
-#            raise ValidationError("user password length should be at least 6")
-#
-#class User(AbstractBaseUser):
-#    name = models.CharField(max_length = 20)
-#    email = models.EmailField(
-#                verbose_name = "Email Address",
-#                max_length = 50,
-#                unique = True,
-#            )
-#
-#    is_active = models.BooleanField(default=True)
-#    is_admin = models.BooleanField(default=False)
-#    is_org_admin = models.BooleanField(default=False)
-#
-#    # custom UserManager
-#    users = UserManager()
-#
--- buildbuild/teams/tests.py
+++ buildbuild/teams/tests.py
@@ -1,54 +1,18 @@
 from django.test import TestCase
+from users.models import User
+from teams.models import Team
 
-# Create your tests here.
-from teams.models import Teams
+#from django.core.exceptions import ObjectDoesNotExist
+#from django .core.exceptions import ValidationError
+#import re
 
-class TeamsModelTest(TestCase):
+class team_composition_test(TestCase):
 	def setUp(self):
-		pass
-	#self.teams = Teams()
-		
+		self.team = Team()
 
-#from users.models import User
-#
-#from django.db import IntegrityError
-#from django.core.exceptions import ValidationError
-#
-#
-#    # Email
-#    def test_user_should_have_email_field(self):
-#        try:
-#            self.user.email
-#        except AttributeError:
-#            self.fail("User should have email field")
-#
-#    def test_user_email_should_not_be_blank(self):
-#        self.user.email = ""
-#        self.user.password = self.valid_password
-#        self.assertRaises(ValueError, self.user.save())
-#
-#    def test_user_should_have_unique_email(self):
-#        self.user.email = self.valid_email
-#        self.user.password = self.valid_password
-#        self.user.save()
-#
-#        user_with_duplicate_email = User(email = self.user.email, password = self.valid_password)
-#
-#        self.assertRaises(IntegrityError, user_with_duplicate_email.save)
-#
-#    def test_user_with_invalid_email_should_not_be_valid(self):
-#        self.assertRaises(ValidationError, User.users.create_user,
-#                email = "INVALID_EMAIL",
-#                password = self.valid_password
-#                )
-#
-#    # Password
-#    def test_user_should_have_password_field(self):
-#        try:
-#            self.user.password
-#        except AttributeError:
-#            self.fail("User should have password field")
-#
-#    def test_user_with_short_password_should_not_be_valid(self):
-#        self.assertRaises(ValidationError, User.users.create_user,
-#                email = self.valid_email,
+	def test_does_team_exists(self):
+		try: 
+			self.team
+		except AttributeError:
+			self.fail("team should exists")
+		pass	

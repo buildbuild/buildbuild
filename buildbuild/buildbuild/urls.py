5661d24e663d8edcde00d253ef673b73fbaec831
dobestan <dobestan@gmail.com>
2014-08-08 16:13:55 +0900

    add very basic home page

--- buildbuild/buildbuild/urls.py
+++ buildbuild/buildbuild/urls.py
@@ -6,11 +6,14 @@ admin.autodiscover()
 from users.views import Login
 from users.views import Logout
 
+from buildbuild.views import Home
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'buildbuild.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', Home.as_view(), name='home'),
     url(r'^login/', Login.as_view(), name="login"),
     url(r'^logout/', Logout.as_view(), name="logout"),
     url(r'^admin/', include(admin.site.urls)),

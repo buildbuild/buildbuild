cfc57a6a90dc20f6d29c5325472735ca028d66a7
riskkim <wnsgh611@gmail.com>
2014-08-13 01:57:32 +0900

    Update urls.py

--- buildbuild/users/urls.py
+++ buildbuild/users/urls.py
@@ -5,6 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
     url(r'^list/$', views.ListAllView.as_view(), name='listAll'),
     url(r'^list/(?P<email>[ a-zA-Z0-9_@\.]+)/$', views.ListOneView.as_view(), name='listOne'),
-    #url(r'^users/', include('users.urls')),
 )
 

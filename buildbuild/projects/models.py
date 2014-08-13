9b96048cc4545a70970c7ec5a1534ce14dee141f
o8151814 <o8151814@gmail.com>
2014-08-13 21:43:55 +0900

    update Project model : project should have unique name

--- buildbuild/projects/models.py
+++ buildbuild/projects/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 class Project(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length = 255, unique = True)
--- buildbuild/projects/tests/test_projects.py
+++ buildbuild/projects/tests/test_projects.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from projects.models import Project
+from django.db import IntegrityError
 
 class TestProject(TestCase):
     def setUp(self):
@@ -11,3 +12,11 @@ class TestProject(TestCase):
             project.name
         except AttributeError:
             self.fail("Project should have name")
+
+    def test_project_should_have_unique_name(self):
+        project = Project()
+        project_with_duplicate_name = Project()
+        project.name = "abc"
+        project.save()
+        project_with_duplicate_name.name = project.name
+        self.assertRaises(IntegrityError, project_with_duplicate_name.save)

c1c7a5ba2c9c969de6f7c46b344a72304a95be3c
o8151814 <o8151814@gmail.com>
2014-08-13 19:49:40 +0900

    make teams.models : add some fields.

--- buildbuild/projects/models.py
+++ buildbuild/projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from teams.models import Team
 
-# Create your models here.
+class ProjectManager(models.Model):
+    def create_project(self, name, owner, src_url, **kwargs):
+
+    def validate_name(self, name):
+        if len(name) > 20
+            raise ValidationError("Project name length should be at most 20")
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=20)
+    own_team = models.ForeignKey(Team)
+    src_url = models.UrlField(max_length=30)
+
+    USERNAME_FIELD = 'name'

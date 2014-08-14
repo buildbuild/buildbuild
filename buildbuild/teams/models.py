c9759c75528ab4d36739e5c9daafb8dbef822fe0
wikibootup <wikibootup@gmail.com>
2014-08-14 15:13:28 +0900

    Add team field validation code about contact number & website url

--- buildbuild/teams/models.py
+++ buildbuild/teams/models.py
@@ -7,8 +7,17 @@ from django.core.exceptions import ValidationError
 class TeamManager(models.Manager):
     def create_team(self, name, **kwargs):
         team = self.model()
-	self.validate_name(name)
+        self.validate_name(name)
         team.name = name
+
+        if "contact_number" in kwargs:
+            self.validate_contact_number(kwargs["contact_number"])
+            team.contact_number = kwargs["contact_number"]
+
+        if "website_url" in kwargs:
+            self.validate_website_url(kwargs["website_url"])
+            team.website_url = kwargs["website_url"]
+
 
         team.save(using = self._db)
         return team
@@ -17,12 +26,22 @@ class TeamManager(models.Manager):
         return Team.objects.all()
 
     def get_team(self, name):
-	self.validate_name(name)
+        self.validate_name(name)
         return Team.objects.get(name = name)
 
     def validate_name(self, name):
-	if len(name) > 30 :
-	    raise ValidationError("Name cannot contain more than 30 characters")
+        if len(name) > 30 :
+            raise ValidationError("Name cannot contain more than 30 characters")
+
+    def validate_contact_number(self, contact_number):
+        if len(contact_number) > 20:
+            raise ValidationError("Contact number cannot contain more than 20 digits")
+
+    def validate_website_url(self, website_url):
+        if len(website_url) > 20:
+            raise ValidationError("Website URL cannot contain more than 80 characters")
+
+
 
 class Team(models.Model):
     """
@@ -35,7 +54,7 @@ class Team(models.Model):
     objects = TeamManager()
     name = models.CharField(max_length = 30)
     contact_number = models.CharField(max_length = 20)
-    website_url = models.URLField(max_length = 50)
+    website_url = models.URLField(max_length = 80)
     users = models.ManyToManyField(User, through = 'Membership')
 
     def __unicode__(self):

6524298f278af5a5d06d160a2fdf1149321c0f79
dobestan <dobestan@gmail.com>
2014-08-19 16:22:04 +0900

    update settings.py : add djcelery in INSTALLED_APPS

--- buildbuild/buildbuild/settings.py
+++ buildbuild/buildbuild/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Python Packages
+    'djcelery',
+
+    # Custom Apps
     'users',
     'teams',
     'projects',

651338b6c6509a8365f9e5413d29b1aa3d91a91f
dobestan <dobestan@gmail.com>
2014-08-29 22:59:31 +0900

    add test : user show page should contain user info

--- buildbuild/users/tests/test_view_show.py
+++ buildbuild/users/tests/test_view_show.py
@@ -5,7 +5,17 @@ from users.models import User
 
 class TestViewShow(TestCase):
     def setUp(self):
-        pass
+        self.user = User.objects.create_user(
+            email = "test_user@example.com",
+            password = "test_password",
+        )
+        self.client = Client()
 
     def test_get_user_show_with_valid_user_id_request_should_return_200(self):
-        pass
+        response = self.client.get("/users/" + str(self.user.id))
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_show_with_valid_user_id_should_contain_user_info(self):
+        response = self.client.get("/users/" + str(self.user.id))
+        self.assertContains(response, self.user.email)
+        self.assertContains(response, self.user.password)

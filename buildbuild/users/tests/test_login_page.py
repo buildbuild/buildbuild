ff55be17cf0b2d26b4281eb9b3dbe9108215a466
dobestan <dobestan@gmail.com>
2014-08-29 22:17:53 +0900

    remove non-REST features

--- buildbuild/users/tests/test_login_page.py
+++ buildbuild/users/tests/test_login_page.py
@@ -29,12 +29,12 @@ class LoginPageTest(TestCase):
             })
         self.assertEqual(response.status_code, 302)
 
-    def test_post_login_page_with_valid_user_information_should_redirect_to_home(self):
+    def test_post_login_page_with_valid_user_information_should_redirect_to_login(self):
         response = self.client.post("/login/", {
             "email": self.valid_email,
             "password": self.valid_password,
             })
-        self.assertEqual(response["Location"], self.TEST_SERVER_URL + "/")
+        self.assertEqual(response["Location"], self.TEST_SERVER_URL + "/login/")
 
     # POST with invalid user information
     def test_post_login_page_with_invalid_user_information_should_return_302(self):
--- buildbuild/users/urls.py
+++ buildbuild/users/urls.py
@@ -2,8 +2,5 @@ from django.conf.urls import patterns, include, url
 from users import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home'),
-    url(r'^list/$', views.ListAllView.as_view(), name='listAll'),
-    url(r'^list/(?P<email>[ a-zA-Z0-9_@\.]+)/$', views.ListOneView.as_view(), name='listOne'),
 )
 

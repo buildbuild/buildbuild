4b1e6389111b040229b4c9989ab8e1d6bce7902f
dobestan <dobestan@gmail.com>
2014-08-29 18:51:41 +0900

    update settings.py : add staticfiles_dirs assets/

--- buildbuild/buildbuild/settings.py
+++ buildbuild/buildbuild/settings.py
@@ -95,6 +95,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',

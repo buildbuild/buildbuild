a033ebb4af825aeedf177c7f6c25aae080f816ed
dobestan <dobestan@gmail.com>
2014-08-29 18:23:42 +0900

    update settings.py : add bower settings and staticfile settings

--- buildbuild/buildbuild/settings.py
+++ buildbuild/buildbuild/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
 
     # Python Packages
     'djcelery',
+    'djangobower',
 
     # Custom Apps
     'users',
@@ -92,6 +93,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'djangobower.finders.BowerFinder',
+)
+
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'components')
+
+BOWER_INSTALLED_APPS = (
+    'angular',
+)
+
 
 AUTH_USER_MODEL = 'users.User'
 

824e2b2b18302dd211cb5df4347374bf94d9c45d
dobestan <dobestan@gmail.com>
2014-08-30 12:43:18 +0900

    add TeamList API

--- buildbuild/api/urls.py
+++ buildbuild/api/urls.py
@@ -3,4 +3,5 @@ from api import views
 
 urlpatterns = patterns('',
     url(r'^users/$', views.UserList.as_view(), name="user-list"),
+    url(r'^teams/$', views.TeamList.as_view(), name="team-list"),
 )
--- buildbuild/api/views.py
+++ buildbuild/api/views.py
@@ -3,7 +3,14 @@ from rest_framework import generics
 from users.models import User
 from users.serializers import UserSerializer
 
+from teams.models import Team
+from teams.serializers import TeamSerializer
+
 
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class TeamList(generics.ListCreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer

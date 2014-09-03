5be545583459fc90092e18ccebccb6e9a6a5034d
riskkim <wnsgh611@gmail.com>
2014-09-03 09:50:12 +0900

    add test setup code for account view

--- buildbuild/users/tests/test_account_view.py
+++ buildbuild/users/tests/test_account_view.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
+from django.test.client import Client
 
 class TestAccountView(TestCase):
     def setUp(self):
-        pass
+        self.user_email = "test@test.com"
+        self.user_password = "12345678"
+        self.user_name = "Name Test"
+        self.user_phone_number = "01012345678"
+
+        self.client = Client()

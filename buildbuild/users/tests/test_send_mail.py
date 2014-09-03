a5338793284262d2f48c328d4847c9431f1a5ab1
wikibootup <wikibootup@gmail.com>
2014-09-04 04:18:48 +0900

    modified test send mail

--- buildbuild/users/tests/test_send_mail.py
+++ buildbuild/users/tests/test_send_mail.py
@@ -17,22 +17,22 @@ class Send_Email_Test(TestCase):
         self.valid_email = "test@example.com"
         self.valid_password = "test_password"
         self.invalid_password = "a"*5
-        self.invalid_host_user = "invalid@gmail.com"
+        self.invalid_email = "invalid@gmail.com"
 
-        # need to find more eloquent way to test redirect url.
-        self.TEST_SERVER_URL = "http://testserver"
+        self.user = User.objects.create_user(
+                email = self.valid_email,
+                password = self.valid_password
+                )
 
-        response = self.client.post("/signup/", {
-            "email": self.valid_email,
-            "password": self.valid_password,
-            })
+    def test_send_mail_to_new_user_using_celery(self):
+        self.assertTrue(tasks.send_mail_to_new_user.delay(self.user))
 
-    def test_send_mail_to_new_user_correctly(self):
+    def test_send_mail_to_new_user(self):
         mail.outbox = []
         mail.send_mail(
                 settings.SUBJECT,
                 settings.CONTENTS,
-                settings.EMAIL_HOST_USER,
+                self.valid_email,
                 [self.valid_email],
                 fail_silently=False
                 )
@@ -43,20 +43,16 @@ class Send_Email_Test(TestCase):
         # Verify that the subject of the first message is correct.
         self.assertEqual(mail.outbox[0].subject, settings.SUBJECT)
 
-    def test_inavalid_host_user_name_cannot_send_mail(self):
+    def test_invalid_email_name_cannot_send_mail(self):
         mail.outbox = []
         mail.send_mail(
                 settings.SUBJECT,
                 settings.CONTENTS,
-                self.invalid_host_user,
+                self.invalid_email,
                 [self.valid_email],
                 fail_silently=False
                 )
                 
         # Test that one message has been sent.
         self.assertEqual(len(mail.outbox), 1)
-
-    def test_send_mail_to_new_user_using_celery(self):
-        mail.outbox = []
-        self.assertTrue(tasks.send_mail_to_new_user.delay())
 

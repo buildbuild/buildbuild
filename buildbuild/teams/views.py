f63f6b96eaf1ca2d3d98b9bd5e985d212a00b60f
wikibootup <wikibootup@gmail.com>
2014-10-14 20:52:00 +0900

    make team class added

--- buildbuild/teams/views.py
+++ buildbuild/teams/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect,request
 
-# Create your views here.
+from django.views.generic.base import RedirectView
+from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
+from django.views.generic import DetailView
+
+from django.contrib.auth import authenticate
+
+from django.contrib import messages
+
+from django.core.urlresolvers import reverse
+
+from django.db import IntegrityError
+from django.db import OperationalError
+from django.core.exceptions import ValidationError
+
+from teams.forms import MakeTeamForm
+from teams.models import Team
+
+class MakeTeamView(FormView):
+    template_name = "teams/maketeam.html"
+    form_class = MakeTeamForm
+
+    def form_valid(self, form):
+        name = self.request.POST["name"]
+
+        try:
+            team = Team.objects.create_team(name)
+        except ValidationError:
+            messages.add_message(
+                    self.request,
+                    messages.ERROR,
+                    "ERROR : invalid information detected"
+                    )
+            return HttpResponseRedirect(reverse("maketeam"))           
+        except IntegrityError:
+            messages.add_message(
+                    self.request, 
+                    messages.ERROR, 
+                    "ERROR : The team name already exists"
+                    )
+            return HttpResponseRedirect(reverse("maketeam"))
+        else: 
+            messages.add_message(
+                    self.request, 
+                    messages.SUCCESS, 
+                    "Team created successfully"
+                    )
+            return HttpResponseRedirect(reverse("home")) 
+

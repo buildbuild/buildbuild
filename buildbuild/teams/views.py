b24a4131a417fe3530f7bf325a70a80a1599dc2f
wikibootup <wikibootup@gmail.com>
2014-10-15 01:00:40 +0900

    integration of make team & membership

--- buildbuild/teams/views.py
+++ buildbuild/teams/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect,request
+from django.http import HttpResponse
 
 from django.views.generic.base import RedirectView
 from django.views.generic.edit import FormView
@@ -14,10 +15,11 @@ from django.core.urlresolvers import reverse
 
 from django.db import IntegrityError
 from django.db import OperationalError
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
 
 from teams.forms import MakeTeamForm
-from teams.models import Team
+from teams.models import Team, Membership
+from users.models import User
 
 class MakeTeamView(FormView):
     template_name = "teams/maketeam.html"
@@ -25,7 +27,7 @@ class MakeTeamView(FormView):
 
     def form_valid(self, form):
         name = self.request.POST["name"]
-
+ 
         try:
             team = Team.objects.create_team(name)
         except ValidationError:
@@ -48,5 +50,9 @@ class MakeTeamView(FormView):
                     messages.SUCCESS, 
                     "Team created successfully"
                     )
-            return HttpResponseRedirect(reverse("home")) 
+            email = self.request.user
+            member = User.objects.get_user(email)
+            member = Membership(team = team, member = member)
+            member.save()
+            return HttpResponseRedirect(reverse("home"))
 

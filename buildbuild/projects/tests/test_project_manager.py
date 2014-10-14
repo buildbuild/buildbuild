f08ad100752a247de46ef2edb311db36d3da7407
wikibootup <wikibootup@gmail.com>
2014-10-15 00:57:29 +0900

    project manager test

--- buildbuild/projects/tests/test_project_manager.py
+++ buildbuild/projects/tests/test_project_manager.py
@@ -7,21 +7,39 @@ from django.core.exceptions import ValidationError
 class TestProjectName(TestCase):
     def setUp(self):
         self.project = Project()
-
-        # Project should belongs to a Team
-        self.team = Team.objects.create_team(name = "test_team_name")
+        self.valid_team_name = "test_team_name"
+        self.valid_project_name = "test_project_name"
+        self.team = Team.objects.create_team(name = self.valid_team_name)
         self.project.team = self.team
 
-    def test_project_should_have_unique_name(self):
-        self.project.name = "test_project_name"
-        self.project.save()
-        project_with_duplicate_name = Project(name = self.project.name)
-        self.assertRaises(IntegrityError, project_with_duplicate_name.save)
-
-    def test_project_name_is_at_most_64_characters(self):
-        self.assertRaises(
-            ValidationError,
-            Project.objects.create_project,
-            name = "a" * 65,
-            team = self.team
+    def test_project_manager_could_create_project(self):
+        self.assertTrue(
+                Project.objects.create_project(
+                    name = self.valid_project_name, 
+                    team = self.team
+                )
         )
+
+    def test_project_should_have_unique_name(self):
+        try:
+            Project.objects.create_project(
+                name = self.valid_project_name, 
+                team = self.team
+            )
+            Project.objects.create_project(
+                name = self.valid_project_name, 
+                team = self.team
+            )
+        except IntegrityError:
+            pass
+    def test_project_name_is_at_most_64_characters(self):
+        try:        
+            Project.objects.create_project(
+                name = "a" * 65,
+                team = self.team,
+            )
+        except ValidationError:
+            pass
+
+
+            

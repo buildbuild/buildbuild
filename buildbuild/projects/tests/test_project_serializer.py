f3dde22cb076cdaf97011254c2364e6c2322ef59
wikibootup <wikibootup@gmail.com>
2014-10-16 14:16:00 +0900

    project rest api modified, project doesn't have team field anymore

--- buildbuild/projects/tests/test_project_serializer.py
+++ buildbuild/projects/tests/test_project_serializer.py
@@ -3,16 +3,10 @@ from django.test import TestCase
 from projects.models import Project
 from projects.serializers import ProjectSerializer
 
-from teams.models import Team
-
-
 class TestProjectSerializer(TestCase):
     def setUp(self):
         self.project = Project.objects.create_project(
             name="test_project_name",
-            team=Team.objects.create_team(
-                name="test_team_name",
-            )
         )
 
     def test_project_serializer_return_serialized_data(self):

331ce31ac932feb9f3ac8c3ea99d583c26dcb740
wikibootup <wikibootup@gmail.com>
2014-10-16 14:11:53 +0900

    project and team is mtom relation

--- buildbuild/projects/models.py
+++ buildbuild/projects/models.py
@@ -3,13 +3,10 @@ from django.core.exceptions import ValidationError
 from teams.models import Team
 
 class ProjectManager(models.Manager):
-    def create_project(self, name, team):
+    def create_project(self, name):
         project = self.model()
-
         self.validate_name(name)
-
         project.name = name
-        project.team = team
 
         project.save(using = self.db)
 
@@ -22,7 +19,49 @@ class ProjectManager(models.Manager):
     def get_project(self, name):
         return Project.objects.get(name = name)
 
+
 class Project(models.Model):
     name = models.CharField(max_length = 64, unique = True)
-    team = models.ForeignKey(Team)
+
     objects = ProjectManager()
+    
+    team_wait_list = models.ManyToManyField(
+            Team, 
+            through = 'ProjectTeamWaitList',
+            through_fields = ('project', 'wait_team'),
+            related_name="project_team_wait_list"
+            )
+    
+    team_list = models.ManyToManyField(
+            Team, 
+            through = 'ProjectTeamList',
+            through_fields = ('project', 'team'),
+            related_name="project_team_list"
+            )
+    
+    def __unicode__(self):
+        return self.name
+
+class ProjectTeamWaitList(models.Model):
+    project = models.ForeignKey(
+            Project, 
+            related_name="project_team_wait_list_project",
+            )
+    wait_team = models.ForeignKey(
+            Team, 
+            related_name="project_team_wait_list_team",
+            )
+    date_requested = models.DateTimeField(auto_now_add=True)
+
+class ProjectTeamList(models.Model):
+    project = models.ForeignKey(
+            Project, 
+            related_name="project_team_list_project",
+            )
+    team = models.ForeignKey(
+            Team, 
+            related_name="project_team_list_team",
+            )
+    date_joined = models.DateField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+

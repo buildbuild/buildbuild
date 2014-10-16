0bcc1962b8463677d5bd83ff40dc5aa742ef0313
wikibootup <wikibootup@gmail.com>
2014-10-16 14:14:24 +0900

    team making from template test

--- buildbuild/projects/tests/test_make_project_page.py
+++ buildbuild/projects/tests/test_make_project_page.py
@@ -11,48 +11,39 @@ from teams.models import Team
 class MakeProjectPageTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.valid_name = "buildbuild_project"
-        self.valid_team_name = "buildbuild_team"
-        self.team = Team.objects.create_team(name = self.valid_team_name)
+        self.name = "buildbuild_project"
+        self.team_name = "buildbuild_team"
+        self.team = Team.objects.create_team(name = self.team_name)
         
         # need to find more eloquent way to test redirect url.
         self.TEST_SERVER_URL = "http://testserver"
+
+        respanse = self.client.post("/makeproject/", {
+            "name":self.name,
+        })
+ 
 
     def test_get_make_project_page_request_should_return_200(self):
         response = self.client.get("/makeproject/")
         self.assertEqual(response.status_code, 200)
 
-    def test_make_project_from_template(self):
-        try:
-            respanse = self.client.post("/makeproject/", {
-                "name":self.valid_name,
-                "team":self.team,
-                })
-        except IntegrityError:
-            pass
- 
     def test_check_uniqueness_of_project_name(self):
-        Project.objects.create_project(
-            name = self.valid_name, 
-            team = self.team
-        )
         try:
             respanse = self.client.post("/makeproject/", {
-                "name":self.valid_name,
-                "team":self.team,
-                })
+                "name":self.name,
+            })
         except IntegrityError:
             pass
     """
     def test_post_available_project_information_return_302(self):
         response = self.client.post("/makeproject/", {
-            "name": self.valid_name,
+            "name": self.name,
             })
         self.assertEqual(response.status_code, 302)
 
     def test_post_available_information_redirect_to_home(self):
         response = self.client.post("/makeproject/", {
-             "name": self.valid_name,
+             "name": self.name,
             })
         self.assertEqual(response["Location"], self.TEST_SERVER_URL + "/")
     """

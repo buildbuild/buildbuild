5d65b7f36af16aace92e56168585899a56f735c2
wikibootup <wikibootup@gmail.com>
2014-10-17 13:08:27 +0900

    text field default value added

--- buildbuild/projects/models.py
+++ buildbuild/projects/models.py
@@ -1,28 +1,56 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from teams.models import Team
+from jsonfield import JSONField
 
 class ProjectManager(models.Manager):
-    def create_project(self, name):
+    def create_project(self, name, **kwargs):
         project = self.model()
         self.validate_name(name)
         project.name = name
+
+        if "properties" in kwargs:
+            project.properties = kwargs['properties']
+        if "docker_text" in kwargs:
+            project.docker_text = kwargs['docker_text']
 
         project.save(using = self.db)
 
         return project
 
     def validate_name(self, name):
-        if len(name) > 64:
-            raise ValidationError("project name is at most 64 chracters")
+        if len(name) > 30:
+            raise ValidationError(
+                ("project name length should be at most 30"),
+                code = 'invalid'
+            )
+        if bool(re.match('^[ a-zA-Z_]+$', name)):
+            pass
+        else:
+            raise ValidationError(
+                "project name cannot contain things but alphabet, white space, '_'"
+            )
 
     def get_project(self, name):
-        return Project.objects.get(name = name)
+        try:
+            self.validate_name(name)
+            project = Project.objects.get(name = name)
+        except ObjectDoesNotExist:
+            raise ObjectDoesNotExist("The project name does not exist")
+        else:
+            return project
+
+    def delete_project(self, name):
+        project = Project.objects.get_project(name)
+        project.deactivate()
+        project.save(using = self._db)
+
 
 
 class Project(models.Model):
-    name = models.CharField(max_length = 64, unique = True)
-
+    name = models.CharField(max_length = 30, unique = True)
+    properties = JSONField()
+    docker_text = models.TextField(default = "none")
     objects = ProjectManager()
     
     team_wait_list = models.ManyToManyField(

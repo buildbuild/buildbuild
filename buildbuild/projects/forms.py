ba7c843b96b0cea52144572ef3c0c8c975d7aca6
wikibootup <wikibootup@gmail.com>
2014-10-18 03:58:45 +0900

    prject form added

--- buildbuild/projects/forms.py
+++ buildbuild/projects/forms.py
@@ -1,6 +1,28 @@
 from django import forms
 
 class MakeProjectForm(forms.Form):
-    name = forms.CharField(max_length = 64, required=True)
+    
+    projects_project_name = forms.CharField(
+            max_length = 64,
+            required=True,
+            label="Project Name(*required)",
+            )
+    """
+    projects_team_name = forms.CharField(
+            max_length = 64,
+            required=False,
+            label="Team Name",
+            )
 
+    lang = forms.CharField(
+            max_length = 20,
+            required=False,
+            label="Language"
+            )
 
+    ver = forms.CharField(
+            max_length = 20,
+            required=False,
+            label="Language Version"
+            )
+    """

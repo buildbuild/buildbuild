91ea8f6bf8378fbedb0209c260f1846c0cde4d48
wikibootup <wikibootup@gmail.com>
2014-10-19 22:28:10 +0900

    team migrations

---
+++ buildbuild/teams/migrations/0006_auto_20141019_1324.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('teams', '0005_auto_20141019_0632'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='team',
+            name='members',
+            field=models.ManyToManyField(related_name=b'member', through='teams.Membership', to=settings.AUTH_USER_MODEL),
+        ),
+    ]

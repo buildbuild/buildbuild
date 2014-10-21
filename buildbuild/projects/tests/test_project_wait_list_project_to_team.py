f06640d509d43974d58d6afee244e245da1c4f68
wikibootup <wikibootup@gmail.com>
2014-10-21 19:52:45 +0900

    team name -> team id

--- buildbuild/projects/tests/test_project_wait_list_project_to_team.py
+++ buildbuild/projects/tests/test_project_wait_list_project_to_team.py
@@ -9,7 +9,7 @@ class project_manager_test(TestCase):
         self.team_name_2 = "Team2"
 
         self.project_name = "Project1"
-        self.project_name_2 = "Project2"
+        self.second_project_name = "Project2"
 
         self.team = Team.objects.create_team(
             name = self.team_name
@@ -23,30 +23,25 @@ class project_manager_test(TestCase):
             name = self.project_name,
         )
         
-        self.project_2 = Project.objects.create_project(
-            name = self.project_name_2,
+        self.second_project = Project.objects.create_project(
+            name = self.second_project_name,
         )
         
-        self.project_wait_list = ProjectWaitList.objects.create(
+        self.project_wait_list = ProjectWaitList.objects.create_project_wait_list(
             project = self.project,
-            wait_team = self.team
+            team = self.team
         )
         self.project_wait_list.save()
         
-    def test_project_could_get_all_team_wait_list(self):
+    def test_project_could_get_all_project_project_wait_teams(self):
         try:
-            self.project.team_wait_list.all()
+            self.project.project_wait_teams.all()
         except:
             self.fail("getting all team list failed")
-    def test_project_could_get_team_wait_list(self):
+    def test_project_could_get_project_project_wait_team(self):
         try:
-            self.project.team_wait_list.get_team(self.team_name)
+            self.project.project_wait_teams.get_project_team(self.team.id)
         except:
             self.fail("getting a wait team from team wait list failed")
 
-    def test_project_wait_list_must_have_date_joined(self):
-        try:
-            self.project_wait_list.date_requested
-        except:
-            self.fail("project_wait_list must have date_requested")
 

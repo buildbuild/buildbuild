447b350b2959e69f67e46de5eb416f0b44dbc5cc
wikibootup <wikibootup@gmail.com>
2014-10-23 20:18:13 +0900

    test HTTP more precise (message, status code, url)

--- buildbuild/users/tests/test_logout.py
+++ buildbuild/users/tests/test_logout.py
@@ -6,23 +6,35 @@ from users.models import User
 class LoginPageTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.valid_email = "test@example.com"
-        self.valid_password = "test_password"
+        self.user_email = "test@example.com"
+        self.user_password = "test_password"
         self.invalid_password = "a"*5
 
         self.user = User.objects.create_user(
-                email = self.valid_email,
-                password = self.valid_password,
+                email = self.user_email,
+                password = self.user_password,
                 )
 
         # need to find more eloquent way to test redirect url.
         self.TEST_SERVER_URL = "http://testserver"
 
+    # Default Set function, These are not Unit Test function
+    def post_login_set(self, user_email="", user_password="", follow = False):
+        response = self.client.post(
+                   "/login/", {
+                       "email" : user_email,
+                       "password" : user_password,
+                       },
+                       follow = follow
+                   )
+        return response
+
+    # Test Code for Default Set function
+    def test_post_login_set(self):
+        self.post_login_set(self.user_email, self.user_password)
+
     def test_logout(self):
-        self.client.post("/login/", {
-            "email": self.valid_email,
-            "password": self.valid_password,
-            })
+        self.post_login_set(self.user_email, self.user_password)
         self.assertRedirects(
             self.client.post("/logout/"),
             "/",

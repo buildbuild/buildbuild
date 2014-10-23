4bcbb0e405ef1ab44f89493248f42f405a853149
wikibootup <wikibootup@gmail.com>
2014-10-23 20:25:41 +0900

    repetitve message -> function message

--- buildbuild/teams/views.py
+++ buildbuild/teams/views.py
@@ -26,50 +26,56 @@ class MakeTeamView(FormView):
     form_class = MakeTeamForm
 
     def form_valid(self, form):
-        # name field required 
-        try:
-            self.request.POST["teams_team_name"]
-        except ObjectDoesNotExist:
+        def msg_error(msg=""):
             messages.add_message(
-                    self.request,
-                    messages.ERROR,
-                    "ERROR : team name form empty"
-                    )
-            return HttpResponseRedirect(reverse("maketeam"))           
-        else:
-            name = self.request.POST["teams_team_name"]
-
-        # valid and unique team name test
-        try:        
-            team = Team.objects.create_team(name)
-        except ValidationError:
-            messages.add_message(
-                    self.request,
-                    messages.ERROR,
-                    "ERROR : invalid team name"
-                    )
-            return HttpResponseRedirect(reverse("maketeam"))          
-        except IntegrityError:
-            messages.add_message(
-                    self.request, 
-                    messages.ERROR, 
-                    "ERROR : The team name already exists"
-                    )
-            return HttpResponseRedirect(reverse("maketeam"))
-        else:
-            # link user to team using Membership  
-            user = self.request.user
-            membership = Membership.objects.create(
-                team = team, 
-                member = user, 
-                is_admin = True,
+                self.request,
+                messages.ERROR,
+                msg
             )
-            membership.save()
- 
+
+        def msg_success(msg=""):
             messages.add_message(
-                    self.request, 
-                    messages.SUCCESS, 
-                    "Team created successfully"
-                    )
-            return HttpResponseRedirect(reverse("home"))
+                self.request,
+                messages.SUCCESS,
+                msg
+            )
+
+        team_invalid = "ERROR : invalid team name"
+        team_already_exist = "ERROR : The team name already exists"
+        team_make_success = "Team created successfully"
+
+        # name field required 
+        name = self.request.POST["teams_team_name"]
+        
+        # valid team name test
+        try:
+            Team.objects.validate_name(name)
+        except ValidationError:
+            msg_error(team_invalid)
+            return HttpResponseRedirect(reverse("maketeam"))          
+
+        # unique team test
+        try:
+            Team.objects.get(name = name)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            msg_error(team_already_exist)
+            return HttpResponseRedirect(reverse("maketeam"))          
+ 
+        # Login check is programmed in buildbuild/urls.py
+        # link user to team using Membership  
+        user = self.request.user
+        team = Team.objects.create_team(name)
+
+        membership = Membership.objects.create(
+            team = team, 
+            member = user, 
+            is_admin = True,
+        )
+        membership.save()
+
+        msg_success(team_make_success)
+ 
+        return HttpResponseRedirect(reverse("home"))
 

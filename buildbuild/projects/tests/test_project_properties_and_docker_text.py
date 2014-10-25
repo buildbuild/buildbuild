a7dbe52983fb8a5db1f5483ec1bb91a93db76ab3
wikibootup <wikibootup@gmail.com>
2014-10-25 18:51:34 +0900

    test project_properties_and_docker_text, For travis test, get using name instead of get id

--- buildbuild/projects/tests/test_project_properties_and_docker_text.py
+++ buildbuild/projects/tests/test_project_properties_and_docker_text.py
@@ -136,6 +136,8 @@ class MakeProjectPageTest(TestCase):
             follow = True
         )
         Language = 0
+        print Project.objects.count()
+        print Project.objects.all()[0].name
         # For travis test, get using name instead of get id
         project = Project.objects.get(name = self.project_name)
         self.assertEqual(project.properties[Language], self.lang_python)

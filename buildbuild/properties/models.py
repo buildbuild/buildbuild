1cb1c060d74aad476aa2b1c751beb4f24e786efb
wikibootup <wikibootup@gmail.com>
2014-10-30 17:29:16 +0900

    version object jsonfield -> 2 charfield (lang, ver) for use more comfortable

--- buildbuild/properties/models.py
+++ buildbuild/properties/models.py
@@ -12,19 +12,14 @@ class Language(models.Model):
 
 
 class VersionManager(models.Manager):
-    def create_ver(self, lang_ver):
-        if type(lang_ver) is not dict:
-            raise ValidationError("lang_ver must be dict")
+    def create_ver(self, lang, ver):
 
-        self.validate_lang(lang_ver.keys())
-        self.validate_ver(lang_ver.values())
+        self.validate_lang(lang)
+        self.validate_ver(ver)
 
-        Version.objects.create(lang_ver)
+        Version.objects.create(lang=lang, ver=ver)
 
     def validate_lang(self, lang):
-        # buildbuild language char field use string field
-        # So <list -> string> using ''.join(object) method
-        lang = ''.join(lang)
         try:
             Language.objects.get(lang = lang)
         except ObjectDoesNotExist:
@@ -32,33 +27,25 @@ class VersionManager(models.Manager):
 
     # It only test available regex for <ver> value, not version service available
     def validate_ver(self, ver):
-        ver = ''.join(ver)
         if bool(re.match('^[0-9.]+$', ver)) is False:
             raise ValidationError(
-                      "version should only be composed of numberic characters and ."
+                      "version should only be composed of numeric characters and ."
                   )
 
     def get_version_list_for_each_language(self, lang):
-        self.validate_lang(lang)
-
-        # dict key type is 'list', for compare to it, string parameter need to be changed to list
-        lang = [lang]
-        all_version_list = Version.objects.all()
-
-        version_list_for_one_language = Version()
-        len_of_all_version_list = len(all_version_list)
-        
-
-        for index in range(len_of_all_version_list):
-            if ver[0].lang_ver.keys() is lang:
-                version_list_for_one_language = ver[0].lang_ver.keys()
-
+        pass
 
 class Version(models.Model):
-    lang_ver = JSONField(
-              help_text = "This field informs available versions for each language",
-              unique = True,
-              default = {"" : ""} # ex : {"python" : "2.7.8"}
+    lang = models.CharField(
+              help_text = "This field informs language",
+              max_length = 30,
+              default = "", # ex : "python"
+          )
+
+    ver = models.CharField(
+              help_text = "This field informs version",
+              max_length = 30,
+              default = "", # ex : "2.7.8"
           )
 
     objects = VersionManager()

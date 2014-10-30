8be027ea06af7eaff613b401dbec3238c3d91e4f
wikibootup <wikibootup@gmail.com>
2014-10-30 16:47:51 +0900

    comment :  postgresql setting for use postgresql

--- buildbuild/buildbuild/settings.py
+++ buildbuild/buildbuild/settings.py
@@ -75,19 +75,30 @@ ROOT_URLCONF = 'buildbuild.urls'
 
 WSGI_APPLICATION = 'buildbuild.wsgi.application'
 
+"""
+# for postgresql
+if "BUILDBUILD_PASSWORD" in os.environ:
+    DB_PASSWORD = os.environ['BUILDBUILD_PASSWORD']
+else:
+    DB_PASSWORD = ""
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'buildbuildDB',
+        'USER': 'buildbuild',
+        'PASSWORD': DB_PASSWORD,
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 

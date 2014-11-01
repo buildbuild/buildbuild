5fc8ad82f6a6fe2da6d1e2d69efa3cbed9574d10
wikibootup <wikibootup@gmail.com>
2014-11-02 05:57:43 +0900

    users app url conf in buildbuild app was moved into users app

--- buildbuild/buildbuild/urls.py
+++ buildbuild/buildbuild/urls.py
@@ -3,29 +3,21 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from users.views import Login
-from users.views import Logout
-from users.views import AccountView
-from users.views import SignUp
 from buildbuild.views import Home
 from teams.views import MakeTeamView
 from projects.views import MakeProjectView
 from django.contrib.auth.decorators import login_required
-from buildbuild import views
+from teams import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'buildbuild.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', Home.as_view(), name='home'),
-    url(r'^search_object/$', views.search_object_form, name='search_object'),
     url(r'^api/', include('api.urls', namespace="api")),
-    url(r'^login/', Login.as_view(), name="login"),
-    url(r'^logout/', Logout.as_view(), name="logout"),
-    url(r'^account/', login_required(AccountView.as_view()), name="account"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('users.urls',namespace='users')),
-    url(r'^signup/', SignUp.as_view(), name="signup"),
     url(r'^maketeam/', login_required(MakeTeamView.as_view()), name="maketeam"),
     url(r'^makeproject/', login_required(MakeProjectView.as_view()), name="makeproject"),
+    url(r'^teams/', include('teams.urls',namespace='teams')),
 )

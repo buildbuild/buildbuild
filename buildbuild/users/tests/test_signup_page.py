092dffe2cd4e86e54d5feec666f54b5a0268651c
wikibootup <wikibootup@gmail.com>
2014-11-02 06:03:52 +0900

    test signup, url : users/signup

--- buildbuild/users/tests/test_signup_page.py
+++ buildbuild/users/tests/test_signup_page.py
@@ -26,7 +26,7 @@ class SignUpPageTest(TestCase):
     # Default Set function, These are not Unit Test function
     def post_signup_set(self, user_email="", user_password="", follow=False):
         response = self.client.post(
-                       "/signup/", {
+                       "/users/signup/", {
                            "email" : user_email,
                            "password" : user_password,
                        },
@@ -39,23 +39,23 @@ class SignUpPageTest(TestCase):
         self.post_signup_set(self.user_email, self.user_password)
 
     def test_get_response_to_signup_page(self):
-        response = self.client.get("/signup/", follow = True)
-        self.assertEqual(response.wsgi_request.path, "/signup/")
+        response = self.client.get("/users/signup/", follow = True)
+        self.assertEqual(response.wsgi_request.path, "/users/signup/")
     
     def test_post_sign_up_user_must_be_unique_and_redirects_to_signup_page_and_error_message(self):
         self.post_signup_set(self.user_email, self.user_password)
         response = self.post_signup_set(self.user_email, self.user_password, follow = True)
-        self.assertRedirects(response, "/signup/",)
+        self.assertRedirects(response, "/users/signup/",)
         self.assertContains(response, self.user_already_exist)
 
     def test_post_available_new_user_information_redirect_to_login_page(self):
         response = self.post_signup_set(self.user_email, self.user_password, follow = True)
-        self.assertRedirects(response, "/login/",)
+        self.assertRedirects(response, "/users/login/",)
         self.assertContains(response, self.user_signup_success)
     
     def test_post_invalid_user_password_return_signup(self):
         response = self.post_signup_set(self.user_email, self.invalid_password, follow = True)
-        self.assertRedirects(response, "/signup/",)
+        self.assertRedirects(response, "/users/signup/",)
         self.assertContains(response, self.user_invalid_password)
     
     # POST with no user information

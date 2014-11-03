412f29e63856aeb33991e85304f5b5a81bd26957
wikibootup <wikibootup@gmail.com>
2014-11-04 08:08:39 +0900

    project url : project page added

--- buildbuild/projects/urls.py
+++ buildbuild/projects/urls.py
@@ -7,4 +7,5 @@ from projects.views import MakeProjectView
 
 urlpatterns = patterns('',
     url(r'^makeproject/', login_required(MakeProjectView.as_view()), name="makeproject"),
+    url(r'^project_page/([0-9]+)/$', 'projects.views.project_page', name='project_page'),
 )

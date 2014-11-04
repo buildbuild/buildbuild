c4211687b85821d548b188847c989fc2013014d9
wikibootup <wikibootup@gmail.com>
2014-11-04 19:17:56 +0900

    teams' forms.py : team name form was deleted, It automatically need to be set

--- buildbuild/projects/forms.py
+++ buildbuild/projects/forms.py
@@ -1,20 +1,17 @@
 from django import forms
 
 class MakeProjectForm(forms.Form):
-    
+    # Make project should be implemented in team page,
+    # not in form field
+    # team name automatically need to be set
+
     projects_project_name = forms.CharField(
             max_length = 64,
             required=True,
             label="Project Name",
             )
-    
-    projects_team_name = forms.CharField(
-            max_length = 64,
-            required=True,
-            label="Team Name",
-            )
-    
-    lang = forms.CharField(
+
+   lang = forms.CharField(
             max_length = 20,
             required=False,
             label="Language"

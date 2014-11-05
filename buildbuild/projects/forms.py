d9ac73bebf65f8189691ec2bba97a4098173ce4c
wikibootup <wikibootup@gmail.com>
2014-11-05 14:47:14 +0900

    indent fixed

--- buildbuild/projects/forms.py
+++ buildbuild/projects/forms.py
@@ -4,14 +4,13 @@ class MakeProjectForm(forms.Form):
     # Make project should be implemented in team page,
     # not in form field
     # team name automatically need to be set
-
     projects_project_name = forms.CharField(
             max_length = 64,
             required=True,
             label="Project Name",
             )
 
-   lang = forms.CharField(
+    lang = forms.CharField(
             max_length = 20,
             required=False,
             label="Language"

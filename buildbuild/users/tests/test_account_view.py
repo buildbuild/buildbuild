1f50380b85543416cb63ae6429b932fb6f7417f6
wikibootup <wikibootup@gmail.com>
2014-11-08 08:02:12 +0900

    users/tests/test_account_view.py : name field must not contain white space, that is applied

--- buildbuild/users/tests/test_account_view.py
+++ buildbuild/users/tests/test_account_view.py
@@ -7,7 +7,7 @@ class TestAccountView(TestCase):
     def setUp(self):
         self.user_email = "test@test.com"
         self.user_password = "12345678"
-        self.user_name = "Name Test"
+        self.user_name = "Name_Test"
         self.user_phone_number = "01012345678"
 
         self.invalid_user_password = "00000000"

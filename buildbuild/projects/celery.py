956ef850f93c83fd401999cdcf67b8c2d4cd1f1f
riskkim <wnsgh611@gmail.com>
2014-11-11 19:55:19 +0900

    add celery task for keystone auth using bypassing method. this method's argument will be deprecated with internal address

--- buildbuild/projects/celery.py
+++ buildbuild/projects/celery.py
@@ -1,7 +1,28 @@
 from __future__ import absolute_import
 
-import os
+from keystoneclient.v2_0 import client
+from keystoneclient.exceptions import AuthSystemNotFound
+from celery import shared_task
 
-from celery import Celery
+@shared_task
+def login_keystone_with_bypass():
+    # For out_network access, this will be deprecated, because we will use this app in intranet.
+    auth_url = 'http://61.43.139.143:5000/v2.0'
+    token = '0120b90111df48feb5c727081afb859f'
+    endpoint = 'http://61.43.139.143:35357/v2.0'
 
-from django.conf import settings
+    # This information will be used real.
+    #auth_url = 'http://172.16.100.169:5000/v2.0'
+    #token = '0120b90111df48feb5c727081afb859f'
+    #endpoint = 'http://172.16.100.169:35357/v2.0'
+
+    keystone = client.Client(
+                    auth_url = auth_url,
+                    token = token,
+                    endpoint = endpoint
+                )
+
+    if keystone is None:
+        raise AuthSystemNotFound("With this information, we can not use keystone service, check information")
+
+    return keystone

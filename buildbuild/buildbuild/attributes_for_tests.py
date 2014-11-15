d04487dce6fe031bc9432b1d5889b82039fe2d99
wikibootup <wikibootup@gmail.com>
2014-11-15 16:08:09 +0900

    buildbuild/attributes_for_tests.py : it is attributes for test

---
+++ buildbuild/buildbuild/attributes_for_tests.py
@@ -0,0 +1,13 @@
+### project apps
+lang_python = 'python'
+ver_python_278 = '2.7.8'
+git_url_buildbuild = 'https://github.com/buildbuild/buildbuild.git'
+branch_name_master = 'master'
+
+properties_for_test = {
+    'language' : lang_python,
+    'version' : ver_python_278,
+    'git_url' : git_url_buildbuild,
+    'branch_name' : branch_name_master,
+}
+

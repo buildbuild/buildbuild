39c6e6fcc67b45c9f61b4751386fd188a920cfa0
korniste <korniste@github.com>
2014-11-15 22:44:02 +0900

    make dockerbuild/build page : version listw

--- buildbuild/dockerbuild/urls.py
+++ buildbuild/dockerbuild/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
-    url(r'^build/([0-9]+)/$', 'dockerbuild.views.build', name='build'),
+    url(r'^/build_page/([0-9]+)/$', 'dockerbuild.views.build_page', name='build_page'),
+    url(r'^/build_page/([0-9]+)/build_new/$', 'dockerbuild.views.build_new', name='build_new'),
 )
--- buildbuild/dockerbuild/views.py
+++ buildbuild/dockerbuild/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from dockerbuild.models import Build
+from projects.models import Project
 
-def build(request, project_id):
-    if "tag_name" in request.GET:
-        tag_name = request.GET['tag_name']
-        build = Build.objects.build_project(project_id=project_id, tag=tag_name)
+def build_page(request, project_id):
+    return render(
+            request,
+            "dockerbuild/build_page.html",
+            { 
+                "project" : Project.objects.get_project(id=project_id),
+                "project_id" : project_id,
+            }   
+    )
+def build_new(request, project_id):
+    if "tag" in request.GET:
+        tag = request.GET['tag']
+        build = Build.objects.build_project(project_id=project_id, tag=tag)
+        project = Project.objects.get_project(project_id)
     else:
         raise ValueError("You must type tag name")
-    return HttpResponse(build.response)
+    return render(
+            request,
+            "dockerbuild/build_new.html",
+            {
+                "project" : project,
+                "build" : build,
+            }
+    ) 

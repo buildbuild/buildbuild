1e01214deae60671f13df6bfbfb8e7ed191e888b
wikibootup <wikibootup@gmail.com>
2014-11-18 15:06:03 +0900

    teams/urls.py : url /teams/ in teams/urls.py, not buildbuild/urls.py

--- buildbuild/teams/urls.py
+++ buildbuild/teams/urls.py
@@ -6,11 +6,11 @@ from django.contrib.auth.decorators import login_required
 from teams.views import MakeTeamView
 
 urlpatterns = patterns('',
-    url(r'^new/', login_required(MakeTeamView.as_view()), name="new"),
-    url(r'^search/$', 'teams.views.search_team', name='search'),
-    url(r'^([0-9]+)/join/$', 'teams.views.join_team', name="join"),
+    url(r'teams/^new/', login_required(MakeTeamView.as_view()), name="new"),
+    url(r'teams/^search/$', 'teams.views.search_team', name='search'),
+    url(r'teams/^([0-9]+)/join/$', 'teams.views.join_team', name="join"),
     url(
-        r'^([0-9]+)/accept/users/([0-9]+)/$', 
+        r'teams/^([0-9]+)/accept/users/([0-9]+)/$', 
         'teams.views.accept_request_to_join_team',
         name = 'accept',
     )

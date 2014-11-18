786f4cef110be4d9fe71bce6fe0627422d5a6ea7
wikibootup <wikibootup@gmail.com>
2014-11-18 15:17:37 +0900

    dockerbuild/urls.py - url name changed : builds:build_new -> builds:new

--- buildbuild/dockerbuild/urls.py
+++ buildbuild/dockerbuild/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     url(r'^([0-9]+)/$', 'dockerbuild.views.build_page', name='build_page'),
-    url(r'^([0-9]+)/new/$', 'dockerbuild.views.build_new', name='build_new'),
+    url(r'^([0-9]+)/new/$', 'dockerbuild.views.build_new', name='new'),
     url(r'^([0-9]+)/deploy/$', 'dockerbuild.views.deploy', name='deploy'),
     url(r'^([0-9]+)/my_project/$', 'dockerbuild.views.my_project', name='my_project'),
 )

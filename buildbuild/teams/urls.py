f5760d2a24f235b569a98adaa3a6fdc858cdfa16
wikibootup <wikibootup@gmail.com>
2014-11-18 15:42:47 +0900

    teams/urls.py : team apps url namespace : teams, team apps urlconf was moved into teams apps' urls.py

--- buildbuild/teams/urls.py
+++ buildbuild/teams/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 from django.contrib.auth.decorators import login_required
-from teams.views import MakeTeamView
+from teams.views import MakeTeamView, TeamList
 
 urlpatterns = patterns('',
     url(r'^teams/new/', login_required(MakeTeamView.as_view()), name="new"),
@@ -13,5 +13,8 @@ urlpatterns = patterns('',
         r'^teams/([0-9]+)/accept/users/([0-9]+)/$', 
         'teams.views.accept_request_to_join_team',
         name = 'accept',
-    )
+    ),
+    url(r'^teams/([0-9]+)/$', 'teams.views.team_page', name='team_page'),
+    url(r'^teams/$', TeamList.as_view(), name="teamlist"),
+
 )   

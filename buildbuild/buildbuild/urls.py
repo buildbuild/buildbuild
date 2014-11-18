139ae27b06873ebbebdb75a89e8ffd75c9633ecb
wikibootup <wikibootup@gmail.com>
2014-11-18 15:05:56 +0900

    buildbuild/urls.py : url /teams/ in teams/urls.py, not buildbuild/urls.py

--- buildbuild/buildbuild/urls.py
+++ buildbuild/buildbuild/urls.py
@@ -7,16 +7,10 @@ from buildbuild.views import Home
 from teams.views import MakeTeamView
 from projects.views import MakeProjectView
 from django.contrib.auth.decorators import login_required
-from teams import views
-from users.views import Login, Logout, \
-    SignUp
 from teams.views import TeamList
 from projects.views import MakeProjectView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'buildbuild.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', Home.as_view(), name='home'),
     url(r'^api/', include('api.urls', namespace="api")),
     url(r'^admin/', include(admin.site.urls)),
@@ -25,7 +19,7 @@ urlpatterns = patterns('',
     url(r'^', include('users.urls', namespace='users')),
     
     # teams' urls
-    url(r'^teams/', include('teams.urls', namespace='teams')),
+    url(r'^', include('teams.urls', namespace='teams')),
     url(r'^teams/$', TeamList.as_view(), name="teamlist"),
     url(r'^teams/([0-9]+)/$', 'teams.views.team_page', name='team_page'),
     

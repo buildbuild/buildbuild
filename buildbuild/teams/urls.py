f48d9054000651cbe187cad70c5f9e693fbd3e8f
wikibootup <wikibootup@gmail.com>
2014-11-18 14:52:09 +0900

    url name : maketeam -> new, accept_request_... -> accept, join_team -> join

--- buildbuild/teams/urls.py
+++ buildbuild/teams/urls.py
@@ -6,12 +6,12 @@ from django.contrib.auth.decorators import login_required
 from teams.views import MakeTeamView
 
 urlpatterns = patterns('',
-    url(r'^new/', login_required(MakeTeamView.as_view()), name="maketeam"),
-    url(r'^search_team/$', 'teams.views.search_team', name='search_team'),
-    url(r'^join_team/([0-9]+)/$', 'teams.views.join_team', name="join_team"),
+    url(r'^new/', login_required(MakeTeamView.as_view()), name="new"),
+    url(r'^search/$', 'teams.views.search_team', name='search'),
+    url(r'^([0-9]+)/join/$', 'teams.views.join_team', name="join"),
     url(
-        r'^accept_request_to_join_team/([0-9]+)-([0-9]+)/$', 
+        r'^([0-9]+)/accept/users/([0-9]+)/$', 
         'teams.views.accept_request_to_join_team',
-        name = 'accept_request_to_join_team',
+        name = 'accept',
     )
 )   

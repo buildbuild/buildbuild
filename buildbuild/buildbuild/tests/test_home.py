17ddeecf2b89fc51dca67748ec975dffe1fe1838
wikibootup <wikibootup@gmail.com>
2014-11-20 18:01:40 +0900

    buildbuild/tests/test_home.py : logout user will be redirectec to /login/?next=

--- buildbuild/buildbuild/tests/test_home.py
+++ buildbuild/buildbuild/tests/test_home.py
@@ -10,14 +10,37 @@ class HomePageTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.factory = RequestFactory()
+        self.user_email = "test@example.com"
+        self.user_password = "test_password"
         self.user = User.objects.create_user(
-                    email = "test@example.com",
-                    password = "test_password",
+                    email = self.user_email,
+                    password = self.user_password,
                 )
 
-    def test_get_home_page_request_should_return_200(self):
+    # Default Set function, These are not Unit Test function
+    def post_login_set(self, user_email="", user_password="", follow = False):
+        response = self.client.post(
+                   "/login/", {
+                       "email" : user_email,
+                       "password" : user_password,
+                       },
+                       follow = follow
+                   )
+        return response
+
+    # Test Code for Default Set function
+    def test_post_login_set(self):
+        self.post_login_set(self.user_email, self.user_password)
+
+    def test_get_home_page_with_login_should_return_200(self):
+        self.post_login_set(self.user_email, self.user_password)
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+
+    def test_get_home_page_without_login_should_return_302_and_redirect_to_login_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/login/?next=/")
 
     def test_home_page_with_no_user_session_should_have_login_text(self):
         response = self.client.get("/")
